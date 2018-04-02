d82eb846ef043bbf0b01fde4fd06f10eb1dba2fe
Tia Sirrine <tiasirrine@outlook.com>
2018-04-02 13:41:26 -0700

    inital

---
+++ portfolio2/assets/javascript/app.js
@@ -0,0 +1,11 @@
+var elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.init(elem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+  console.log("sanity");
